4137b8bdea61047b91084d30661b76345f04dba7
Erin Clark <erincodepirate@gmail.com>
2023-02-03 15:53:36 -0800

    Add initial John Brown prototype demo

---
+++ main.py
@@ -0,0 +1,93 @@
+import arcade
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
+
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+
+PLAYER_MOVEMENT_SPEED = 5
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+        self.scene = None
+        self.player_sprite = None
+
+        self.physics_engine = None
+
+    def setup(self):
+        self.scene = arcade.Scene()
+
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
+
+        image_source = "images/johnbrown.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.scene.add_sprite("Player", self.player_sprite)
+
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(
+                ":resources:images/tiles/grassMid.png", 
+                TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.scene.add_sprite("Walls", wall)
+
+        coordinate_list = [
+            [512, 256], [256, 256], [768, 256],
+            [512, 96], [256, 96], [768, 96]
+            ]
+
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", 
+                TILE_SCALING)
+            wall.position = coordinate
+            self.scene.add_sprite("Walls", wall)
+
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player_sprite, self.scene.get_sprite_list("Walls")
+        )
+
+    def on_draw(self):
+        self.clear()
+
+        self.scene.draw()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        self.physics_engine.update()
+
+def main():
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
